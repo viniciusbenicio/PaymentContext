38072530086604ce7b440c99f2136347c837eceb
Vinicius Benicio <vinicius.benicio97@gmail.com>
2023-11-28 23:50:19 -0300

    Implementando Validações e utilizando a biblioteca Flunt

--- PaymentContext.Tests/Entities/StudentTests.cs
+++ PaymentContext.Tests/Entities/StudentTests.cs
@@ -1,4 +1,4 @@
-﻿using PaymentContext.Domain.Entities;
+﻿using PaymentContext.Domain.ValueObjects;
 
 namespace PaymentContext.Tests;
 
@@ -8,8 +8,11 @@ public class StudentTests
     [TestMethod]
     public void AdicionarAssinatura()
     {
-        //var subscription = new Subscription(null);
-        //var student = new Student("Vinicius", "Benicio de Santana", "1221212", "viniciusbenicio@email.com");
-        //student.AddSubscription(subscription);
+        var name = new Name("Teste", "Teste");
+
+        foreach(var not in name.Notifications)
+        {
+            not.Message = not.Message;
+        }
     }
 }
