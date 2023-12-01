954db7629ae41569288461c3452eb6244655af72
Vinicius Benicio <vinicius.benicio97@gmail.com>
2023-11-30 22:51:38 -0300

    Testando os Commands

---
+++ PaymentContext.Tests/Command/CreateBoletoSubscriptionCommandTests.cs
@@ -0,0 +1,18 @@
+﻿
+using PaymentContext.Domain.Commands;
+
+namespace PaymentContext.Tests.Command
+{
+    [TestClass]
+    public class CreateBoletoSubscriptionCommandTests
+    {
+        [TestMethod]
+        public void ShouldReturnErrorWhenNameIsInvalid()
+        {
+            var command = new CreateBoletoSubscriptionCommand();
+            command.FirstName = "";
+            command.Validate();
+            Assert.AreEqual(false, command.IsValid);
+        }
+    }
+}
