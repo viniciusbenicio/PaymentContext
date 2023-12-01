4e4c3199f27acd388a583cc6aa599212565f9dda
Vinicius Benicio <vinicius.benicio97@gmail.com>
2023-12-01 20:59:22 -0300

    Testando os Handlers

---
+++ PaymentContext.Tests/Handlers/SubscriptionHandlerTests.cs
@@ -0,0 +1,48 @@
+﻿using PaymentContext.Domain.Commands;
+using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Handlers;
+using PaymentContext.Tests.Mocks;
+using PaymentContext.Domain.Enums;
+
+namespace PaymentContext.Tests.Handlers
+{
+
+    [TestClass]
+    public class SubscriptionHandlerTests
+    {
+        [TestMethod]
+        public void ShouldReturnErrorWhenDocumentExists()
+        {
+            var handler = new SubscriptionHandler(new FakeStudentRepository(), new FakeEmailServices());
+            var command = new CreateBoletoSubscriptionCommand();
+            command.FirstName = "Vinicius";
+            command.LastName = "Benicio de Santana";
+            command.Document = "9999999999";
+            command.Email = "hello@microsoft.com";
+            command.BarCode = "1212121212";
+            command.BoletoNumber = "1212121212";
+            command.PaymentNumber = "1212121212";
+            command.PaidDate = DateTime.Now;
+            command.ExpireDate = DateTime.Now.AddMonths(1);
+            command.Total = 60;
+            command.TotalPaid = 60;
+            command.Payer = "Microsoft";
+            command.PayerDocument = "121212121212";
+            command.PayerDocumentType = EDocumentType.CPF;
+            command.PayerEmail = "hello@microsoft.com";
+            command.Street = "asjasas";
+            command.Number = "1212";
+            command.Neighborhood = "asasas";
+            command.City = "asas";
+            command.State = "asas";
+            command.Country = "asas";
+            command.ZipCode = "98218912893123";
+
+            handler.Handle(command);
+            Assert.AreEqual(false, command.IsValid);
+
+
+        }
+    }
+
+}
---
+++ PaymentContext.Tests/Mocks/FakeStudentRepository.cs
@@ -0,0 +1,26 @@
+﻿using PaymentContext.Domain.Entities;
+using PaymentContext.Domain.Repositories;
+
+namespace PaymentContext.Tests.Mocks
+{
+    public class FakeStudentRepository : IStudentRepository
+    {
+        public void CreateSubscription(Student student) { }
+
+        public bool DocumentExists(string document)
+        {
+            if (document == "9999999999")
+                return true;
+
+            return false;
+        }
+
+        public bool EmailExists(string email)
+        {
+            if (email == "hello@microsoft.com")
+                return true;
+
+            return false;
+        }
+    }
+}
